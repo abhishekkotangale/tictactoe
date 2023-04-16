74a5fb1ab23b0db056f1a16d089b3978a71242ff
abhishek <abhishekkotangale100@gmail.com>
2023-04-17 03:50:30 +0530

    added sass

--- src/components/Sqaure.js
+++ src/components/Sqaure.js
@@ -1,8 +1,8 @@
-import React from 'react'
+import React from 'react';
 
 const Sqaure = ({value}) => {
   return (
-    <button type="button">{value}</button>
+    <button type="button" className='square'>{value}</button>
   )
 }
 
