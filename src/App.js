9fb21d6c4862e771b187d199a6d69065a6850cde
abhishek <abhishekkotangale100@gmail.com>
2023-04-17 14:48:39 +0530

    calculate winner

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
-import React from 'react';
+import React ,{useState}from 'react';
 import Board from './components/Board';
 
 import "./styles/root.scss";
+import { calculateWinner } from './Helpers';
 
 const App = () => {
+
+  const [ board , setBoard] = useState(Array(9).fill(null));
+  const [isXnext , setIsNext] = useState(false);
+    
+  const winner = calculateWinner(board);
+
+  const message = winner ? 
+  `Winner id ${winner}`
+  : `Next Player is ${isXnext ? 'X' : 'O'}`;
+    const handleSquareClick = (position) => {
+        if(board[position] || winner){
+            return;
+        }
+        setBoard( (prev) => {
+            return prev.map((square , pos) => {
+                if(pos === position){
+                    return isXnext ? 'X' : 'O';
+                }
+
+                return square;
+            })
+        });
+        setIsNext(prev => !prev);
+    };
+
   return (
     <div className='app'>
       <h1>Tic Tac Toe</h1>
-      <Board />
+    <h2>{message}</h2>
+      <Board board={board} handleSquareClick={handleSquareClick}/>
     </div>
   )
 }
--- src/components/Board.js
+++ src/components/Board.js
@@ -1,27 +1,7 @@
-import React , { useState } from 'react';
+import React from 'react';
 import Sqaure from './Sqaure';
 
-const Board = () => {
-    const [ board , setBoard] = useState(Array(9).fill(null));
-    const [isXnext , setIsNext] = useState(false);
-    
-
-    const handleSquareClick = (position) => {
-        if(board[position]){
-            return;
-        }
-        setBoard( (prev) => {
-            return prev.map((square , pos) => {
-                if(pos === position){
-                    return isXnext ? 'X' : 'O';
-                }
-
-                return square;
-            })
-        });
-        setIsNext(prev => !prev);
-    };
-
+const Board = ({board,handleSquareClick}) => {
     
 
     const renderSquare = position => {
