79516de959472ab26e82ed2eb0e73aee42c4fdb2
abhishek <abhishekkotangale100@gmail.com>
2023-04-17 10:31:32 +0530

    added board state and event

--- src/components/Sqaure.js
+++ src/components/Sqaure.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
-const Sqaure = ({value}) => {
+const Sqaure = ({value , onClick}) => {
   return (
-    <button type="button" className='square'>{value}</button>
+    <button
+        type="button"
+        className='square'
+        onClick={onClick}
+    >
+        {value}
+    </button>
   )
 }
 
